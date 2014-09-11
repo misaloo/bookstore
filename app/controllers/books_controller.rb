c2eebc0c3528ca87bf80ebb2605f34713ab7ff48
Lisa Lowe <lisa.s.lowe@gmail.com>
2014-09-11 19:30:38 -0400

    fixed books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 class BooksController < ApplicationController
 	def index
-		@available_at=Time.now
+		@available_at = Time.now
 
-		@books=["Atlas Shrugged","Ender's Game","Hamlet","The Hobbit"]
+		@books = Book.all
 	end
 	def show
   		@book = Book.find(params[:id])
