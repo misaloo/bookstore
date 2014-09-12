bd188438586abd333abf4c6087db021455bffaf3
Lisa Lowe <lisa.s.lowe@gmail.com>
2014-09-11 21:06:41 -0400

    week 3 lab 6 - resource routes and partials

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
+	before_action :set_book, only: [ :show, :edit, :update, :destroy ]
 	def index
 		@available_at = Time.now
 
 		@books = Book.all
 	end
 	def show
-  		@book = Book.find(params[:id])
 	end
 	def new
 	  @book = Book.new
@@ -18,16 +18,13 @@ class BooksController < ApplicationController
 	end
 
 	def edit
-	  @book = Book.find(params[:id])
 	end
 
 	def update
-	  @book = Book.find(params[:id])
 	  @book.update(book_params)
 	  redirect_to @book
 	end
 	def destroy
-	  @book = Book.find(params[:id])
 	  @book.destroy
 	  redirect_to books_url
 	end
@@ -35,5 +32,8 @@ class BooksController < ApplicationController
 
 	def book_params
 	  params.require(:book).permit(:title, :author, :pages, :price)
+	end
+	def set_book
+  		@book = Book.find(params[:id])
 	end
 end
