f7c36d09cc58c35e7b17e423a8df2e6d0235b0b1
Lisa Lowe <lisa.s.lowe@gmail.com>
2014-10-02 19:34:34 -0400

    Week 5 Labs 1-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bookstore::Application.routes.draw do
+  resources :reviews
+
   #get "/books" => "books#index", as: 'books'
   #get "/books/new" => "books#new", as: 'new_book'
   #get "/books/:id" => "books#show", as: 'book'
@@ -6,6 +8,9 @@ Bookstore::Application.routes.draw do
   #get "/books/:id/edit" => "books#edit", as: 'edit_book'
   #patch "/books/:id" => "books#update"
   #delete "/books/:id" => "books#destroy"
-  resources :books
+  resources :books do
+    resources :reviews
+    get 'page/:page', :action => :index, :on => :collection
+  end
   root 'books#index'
 end
